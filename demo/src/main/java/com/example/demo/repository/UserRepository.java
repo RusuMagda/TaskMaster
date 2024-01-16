308aca82ba856fc2831eadec18edc454205a8328
nicoletacater <156836248+nicoletacater@users.noreply.github.com>
2024-01-16 20:05:51 +0200

    Update UserRepository.java

--- demo/src/main/java/com/example/demo/repository/UserRepository.java
+++ demo/src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,10 +1,12 @@
-package com.example.demo.repository;
+package com.backend.dam.repository;
 
-
-import com.example.demo.model.User;
+import com.backend.dam.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    Optional<User> findByEmail(String username);
 }
