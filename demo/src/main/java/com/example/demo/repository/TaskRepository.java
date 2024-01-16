b5575ea47dd5fea5d9c6f60d9f717c5de33f7f65
AndreeaHrestic <156835716+AndreeaHrestic@users.noreply.github.com>
2024-01-16 20:05:01 +0200

    Update TaskRepository.java

--- demo/src/main/java/com/example/demo/repository/TaskRepository.java
+++ demo/src/main/java/com/example/demo/repository/TaskRepository.java
@@ -1,10 +1,14 @@
-package com.example.demo.repository;
+package com.backend.dam.repository;
 
-
-import com.example.demo.model.Task;
+import com.backend.dam.model.Status;
+import com.backend.dam.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task,Long> {
+    List<Task> findByUsersEmail(String email);
+    List<Task> findByUsersEmailAndStatus(String userEmail, Status status);
 }
