7adcd402099f0bd5c2200ff71b93395896ee63b5
LaraCasian <150183523+LaraCasian@users.noreply.github.com>
2024-01-16 20:03:57 +0200

    Update ProjectRepository.java

--- demo/src/main/java/com/example/demo/repository/ProjectRepository.java
+++ demo/src/main/java/com/example/demo/repository/ProjectRepository.java
@@ -1,9 +1,16 @@
-package com.example.demo.repository;
+package com.backend.dam.repository;
 
-import com.example.demo.model.Project;
+import com.backend.dam.model.Project;
+import com.backend.dam.model.Status;
+import com.backend.dam.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    List<Project> findProjectsByStatus(Status status);
 }
